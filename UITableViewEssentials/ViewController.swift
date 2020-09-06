d1dd125cb7bafe6e89c7b1be17175ce18b77a98f
Juke Jaster <justin@justncode.com>
2020-09-05 20:07:31 -0400

    Add base UITableViewController

--- UITableViewEssentials/ViewController.swift
+++ UITableViewEssentials/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    private let contacts = [
+        "Dan",
+        "Joan",
+        "John",
+        "Chris"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
+
+// MARK: - UITableViewDataSource
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        cell.textLabel?.text = contacts[indexPath.row]
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ViewController { }
 
