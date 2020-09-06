b2fa48ca893b4581b7b35944f8d11459266783a2
Juke Jaster <justin@justncode.com>
2020-09-06 02:20:01 -0400

    Initial Commit

---
+++ UITableViewEssentials/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewEssentials
+//
+//  Created by Juke Jaster on 9/6/20.
+//  Copyright © 2020 Juke Jaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
